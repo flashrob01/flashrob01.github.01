8306e55b59bfaf0ea6486e313e5268f2a17f9ca9
Rob L <81087205+flashrob01@users.noreply.github.com>
2021-12-27 15:02:36 +0800

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect, Component} from 'react';
 
-import DOMPurify from'dompurify';
-
 import {Link} from 'react-router-dom';
 
 import { useAuth0 } from "@auth0/auth0-react";
