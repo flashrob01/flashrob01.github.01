503ab69d442ea4abcac00be8c5f1bb19b5270d88
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-01-17 08:42:39 +0800

    01172022
    more minor changes

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,7 @@
 
 
 const API_Endpoint = process.env.NODE_ENV === 'production'
-  ? 'https://sunshine-store.herokuapp.com'
+  ? 'https://ddrc-app.herokuapp.com'
   : 'http://localhost:4000';
 
 export default API_Endpoint;
