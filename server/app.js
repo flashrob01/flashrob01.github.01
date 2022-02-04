41c9ce8e906fdf5416d8109a484f4b633daa2f58
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-02-04 21:54:27 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -46,10 +46,10 @@ app.use(cors(corsOptions));
 //copied from sunshine-server==
 
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "https://ddrc-app.herokuapp.com");
+ res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "application/x-www-form-urlencoded");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Origin', '*');
+  
  /*  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', "application/x-www-form-urlencoded");
   res.header('Access-Control-Allow-Credentials', true); */
