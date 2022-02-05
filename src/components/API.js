7b64f7dfc420f545a88f967e950311b3452988e1
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-02-05 21:47:47 +0800

    Update API.js

--- src/components/API.js
+++ src/components/API.js
@@ -1,3 +1,3 @@
-export default "postgres://jenmayquzeprgw:8c854577b12b3fa95d882de2ce8b2b4cdb7f777ebf74530583f757891e9228a3@ec2-34-205-217-14.compute-1.amazonaws.com:5432/dce5a6da9jem9u";
+export default "postgres://jenmayquzeprgw:8c854577b12b3fa95d882de2ce8b2b4cdb7f777ebf74530583f757891e9228a3@ec2-34-205-217-14.compute-1.amazonaws.com:5432/dce5a6da9jem9u?ssl=true";
 
 //This is necessary because React automatically assumes the PG database is sitting at port 3000
