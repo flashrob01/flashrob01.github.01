220f48645b2ebf34777296dafac86abc8fccb3d9
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-02-05 08:30:58 +0800

    Update API.js

--- src/components/API.js
+++ src/components/API.js
@@ -1,3 +1,3 @@
-export default "process.env.DATABASE_URL";
+export default process.env.DATABASE_URL;
 
 //This is necessary because React automatically assumes the PG database is sitting at port 3000
