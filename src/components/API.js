c5c14566300995f3ba6e9c225ede756d1549dee7
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-02-07 08:41:04 +0800

    Update API.js

--- src/components/API.js
+++ src/components/API.js
@@ -1,3 +1,3 @@
-export default "https://ddrc-app.herokuapp.com";
+export default "https://bright-mullet-79.hasura.app/v1/graphql";
 
 //This is necessary because React automatically assumes the PG database is sitting at port 3000
