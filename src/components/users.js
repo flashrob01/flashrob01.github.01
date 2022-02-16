315957361c5a715350263beac0c62b76e803ac8a
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-02-16 09:12:02 +0800

    Add files via upload

---
+++ src/components/users.js
@@ -0,0 +1,40 @@
+import { useApi } from './use-api';
+
+ const Dudu = () => {
+  const opts = {
+    audience: 'https://bright-mullet-79.hasura.app/v1/graphql/',
+    scope: 'read:users',
+  };
+  const { login, getAccessTokenWithPopup } = useAuth0();
+  const { loading, error, refresh, data: users } = useApi(
+    'https://bright-mullet-79.hasura.app/v1/graphql/users',
+    opts
+  );
+  const getTokenAndTryAgain = async () => {
+    await getAccessTokenWithPopup(opts);
+    refresh();
+  };
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    if (error.error === 'login_required') {
+      return <button onClick={() => login(opts)}>Login</button>;
+    }
+    if (error.error === 'consent_required') {
+      return (
+        <button onClick={getTokenAndTryAgain}>Consent to reading users</button>
+      );
+    }
+    return <div>Oops {error.message}</div>;
+  }
+  return (
+    <ul>
+      {users.map((user, index) => {
+        return <li key={index}>{user}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default Dudu;
