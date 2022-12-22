a2dd53b544f80d41534355169deee5f8a8b53d78
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-12-22 22:17:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ ReactDOM.render(
     // redirectUri='http://localhost:3000'
 
     //Note- unfreeze above and freeze below when going from production to local testing!
-    redirectUri="https://ddr-store.herokuapp.com/"
+    redirectUri="https://www.store.robliou.com/"
     audience="hasura"
   >
     <AuthorizedApolloProvider graphQLClientConfig={{ cache }}>
